4fc433e9efb742b9a87f75ee3b9e6ec5a38deb6a
Rae <rae.rudie@gmail.com>
2020-10-05 13:38:04 -0400

    remove unused material ui component

--- src/frontend/components/App.jsx
+++ src/frontend/components/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import Cookies from 'js-cookie';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { lazy, LazyBoundary } from 'react-imported-component';
-import Container from 'react-bootstrap/Container';
 import { ErrorBoundary } from 'react-error-boundary';
 import 'react-form-builder2/dist/app.css';
 import Error from './Error.jsx';
